10cde10201742718da92833c3dd2fe78ec091e1e
RACHEL BROCHADO <rachel.brochado@gmail.com>
2020-02-24 18:42:16 +0100

    frontend stuff

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
 		return (
-			<p>AboutPage Works!</p>
+			<div>
+				<Hero title={props.title} />
+
+				<Content>
+				<p> Hello, I'm Rachel Brochado.
+				<p>Before beginning my career in the tech industry in Germany, I finished my Masters in Sustainability Science and Policy in Maastricht, Netherlands.</p>
+				<p>My main goal is to be able to spread the word for what I believe, and to support more fair and socially responsible technologies.</p>
+				<p>I believe in  technology to recreate the necessary balance of the health of our nature and planet, in the power of communities, and freedom as fuel to bring change in human behavior.</p>
+				<p>Besides all of that, I'm very interested in biking, live music and in getting better at computer programming (atm React!).</p>
+				</p>
+
+				</Content>
+			</div>
 
 		);
 }
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,78 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function ContactPage(props) {
-		return (
-			<p>ContactPage Works!</p>
+class ContactPage extends React.Component {
+		constructor(props) {
+				super(props);
+				this.state = {
+					name: '',
+					email: '',
+					message: '',
+					disabled: false,
+					emailSent: null,
+				}
+		}
+
+		handleChange = (event) => {
+
+			const target = event.target;
+			const value = target.type === 'checkbox' ? target.checked : target.value;
+			const name = target.name;
+
+			this.setState({
+					[name]: value
+			})
+		}
+
+		handleSubmit = (event) => {
+				event.preventDefault();
+
+				this.setState({
+						disabled: true,
+		
+				})
+		}
+
+
+
+		render () {
+			return (
+			<div>
+				<Hero title={this.props.title}/>
+
+				<Content>
+						<Form onSubmit={this.handleSubmit}>
+							<Form.Group>
+								<Form.Label htmlFor="full-name">full name</Form.Label>
+								<Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+							</Form.Group>
+
+							<Form.Group>
+								<Form.Label htmlFor="email">email</Form.Label>
+								<Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+							</Form.Group>
+
+							<Form.Group>
+								<Form.Label htmlFor="message">message</Form.Label>
+								<Form.Control id="message" name="message" as="textarea" rows="4" value={this.state.message} onChange={this.handleChange} />
+							</Form.Group>
+
+							<Button className="d-inline-block" variant="primary" typ="submit" disabled={this.state.disabled}>
+							Send
+							</Button>
+
+							{this.state.emailSent === true && <p className="d-inline success-msg">email sent</p>}
+							{this.state.emailSent === false && <p className="d-inline err-msg">email NOT sent</p>}
+
+						</Form>
+				</Content>
+			</div>
 
 		);
+	}
 }
 
 export default ContactPage;
